a276faf1faf269961a738837766872256acb1328
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-12-25 15:36:47 -0300

    feat: add app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth";
+import Routes from "./routes";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={""} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
