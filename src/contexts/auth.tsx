55d0b08b14139275150facfc72d78d94b30ec014
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-12-25 15:35:55 -0300

    feat: create context, service and page to sign in

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, ReactNode, useState } from "react";
+import * as auth from "../services/auth";
+
+interface AuthContextData {
+  signed: boolean;
+  token?: string;
+  user?: object | null;
+  signIn(): Promise<void>;
+}
+interface ProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC<ProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<object | null>(null);
+
+  async function signIn() {
+    const response = await auth.signIn();
+    setUser(response.user);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signed: !!user, user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import AuthContext from "../../contexts/auth";
+
+const SignIn: React.FC = () => {
+  const { signIn } = useContext(AuthContext);
+
+  // console.log(signed)
+  async function handleSignIn() {
+    await signIn();
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleSignIn}>
+        Sign In
+      </button>
+    </div>
+  );
+};
+
+export default SignIn;
